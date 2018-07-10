b3da42208d59a3fe800ce5f39834bdc8f75eef4a
Hazar <arif.aliyev@gmail.com>
2018-07-09 23:46:11 -0500

    Homework

---
+++ keys.js
@@ -0,0 +1,15 @@
+
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: process.tD6sTEXyGX8fNuzGLeNjibW9s,
+  consumer_secret: process.env.mw8KjFDM42uFkuOrk1vDwqONLcXHxdY6cLZtk6ih6cx4lcRUvc,
+  access_token_key: process.env.1015316199025102848-n9zXGEmpX5s6nylKMEZRtQL8eLoPx1,
+  access_token_secret: process.env.YApL2l4hjKxkj7VSxY3vgl3QR83KUlX2EObsXYTZN9bIO
+
+};
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
