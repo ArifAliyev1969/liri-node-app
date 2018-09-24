237e7280adab35e23d7baff4fcf2e5476787b01b
Hazar <arif.aliyev@gmail.com>
2018-09-24 14:25:46 -0500

    Homework

--- keys.js
+++ keys.js
@@ -1,12 +1,8 @@
-
-console.log('this is loaded');
-
 exports.twitterKeys = {
-  consumer_key: process.tD6sTEXyGX8fNuzGLeNjibW9s,
-  consumer_secret: process.env.mw8KjFDM42uFkuOrk1vDwqONLcXHxdY6cLZtk6ih6cx4lcRUvc,
-  access_token_key: process.env.1015316199025102848-n9zXGEmpX5s6nylKMEZRtQL8eLoPx1,
-  access_token_secret: process.env.YApL2l4hjKxkj7VSxY3vgl3QR83KUlX2EObsXYTZN9bIO
-
+  consumer_key: "tD6sTEXyGX8fNuzGLeNjibW9s",
+  consumer_secret: "mw8KjFDM42uFkuOrk1vDwqONLcXHxdY6cLZtk6ih6cx4lcRUvc",
+  access_token_key: "1015316199025102848-n9zXGEmpX5s6nylKMEZRtQL8eLoPx1",
+  access_token_secret: "YApL2l4hjKxkj7VSxY3vgl3QR83KUlX2EObsXYTZN9bIO"
 };
 
 exports.spotify = {
